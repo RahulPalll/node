ade5d8e5c78f5c68bc8cdbcace26d675175ed7a1
Rahul Pal <pal44rahul@gmail.com>
2024-11-08 08:00:04 +0530

    explicit import and export

---
+++ app.js
@@ -0,0 +1,10 @@
+require('./xyz.js');
+require('./sum.js');
+const calculateSum = require('./sum.js');
+
+
+var name = "namaste Nodejs";
+
+var a=10;
+var b=20;
+calculateSum(a,b);
---
+++ sum.js
@@ -0,0 +1,10 @@
+console.log("sum module executed");
+
+var x = "hello world  ";
+
+function calculateSum(a, b) {
+  sum = a + b;
+  console.log(sum);
+}
+
+module.exports = calculateSum;
