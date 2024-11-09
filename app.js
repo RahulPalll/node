e144513f0dfde7a193274ecb144f8bfaf24f1549
Rahul Pal <pal44rahul@gmail.com>
2024-11-09 08:39:53 +0530

    json data access

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-// require("./xyz.js");
-// require("./sum.js");
-// const obj = require("./sum.js");
-
-// var name = "namaste Nodejs";
-
-// var a = 10;
-// var b = 20;
-// obj.calculateSum(a, b);
-// console.log(obj.x);
-
-// require("./xyz.js");
-// require("./sum.js");
-import { calculateSum, x } from "./sum.js";
+require("./xyz.js");
+require("./sum.js");
+const obj = require("./sum.js");
+const data = require("./data");
 
 var name = "namaste Nodejs";
 
 var a = 10;
 var b = 20;
-calculateSum(a, b);
-console.log(x);
+obj.calculateSum(a, b);
+console.log(obj.x);
+
+require("./data");
+
+console.log(data);
+
+// import { calculateSum, x } from "./sum.js";
+
+// var name = "namaste Nodejs";
+
+// var a = 10;
+// var b = 20;
+// calculateSum(a, b);
+// console.log(x);
--- sum.js
+++ sum.js
@@ -1,21 +1,21 @@
-// console.log("sum module executed");
-
-// var x = "hello world  ";
-
-// function calculateSum(a, b) {
-//   sum = a + b;
-//   console.log(sum);
-// }
-
-// module.exports = { x: x, calculateSum: calculateSum };
-
 console.log("sum module executed");
 
-export var x = "hello world  ";
+var x = "hello world  ";
 
-export function calculateSum(a, b) {
-  var sum = a + b;
+function calculateSum(a, b) {
+  sum = a + b;
   console.log(sum);
 }
+
+module.exports = { x: x, calculateSum: calculateSum };
+
+// console.log("sum module executed");
+
+// export var x = "hello world  ";
+
+// export function calculateSum(a, b) {
+//   var sum = a + b;
+//   console.log(sum);
+// }
 
 // module.exports = { x: x, calculateSum: calculateSum };
