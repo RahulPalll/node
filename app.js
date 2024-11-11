88acd4024514b378fd5d0ee7804457eec551bd43
Rahul Pal <pal44rahul@gmail.com>
2024-11-11 08:41:54 +0530

    promises

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-require("./xyz.js");
-require("./sum.js");
-const obj = require("./sum.js");
-const data = require("./data");
+// require("./xyz.js");
+// require("./sum.js");
+// const obj = require("./sum.js");
+// const data = require("./data");
 
-var name = "namaste Nodejs";
+// var name = "namaste Nodejs";
 
-var a = 10;
-var b = 20;
-obj.calculateSum(a, b);
-console.log(obj.x);
+// var a = 10;
+// var b = 20;
+// obj.calculateSum(a, b);
+// console.log(obj.x);
 
-require("./data");
+// require("./data");
 
-console.log(data);
+// console.log(data);
 
 // import { calculateSum, x } from "./sum.js";
 
@@ -22,3 +22,22 @@ console.log(data);
 // var b = 20;
 // calculateSum(a, b);
 // console.log(x);
+
+let promise = new Promise((resolve, reject) => {
+  const success = true;
+  setTimeout(() => {
+    if (resolve) {
+      resolve("Promise Resolved");
+    } else {
+      reject("Promise rejected");
+    }
+  }, 10);
+});
+
+promise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
