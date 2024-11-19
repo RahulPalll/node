be46ac44c826f30534e419f1e74172ff7317651a
Rahul Pal <pal44rahul@gmail.com>
2024-11-20 01:23:57 +0530

    check

--- dsa/nonrepeating.js
+++ dsa/nonrepeating.js
@@ -1,17 +1,16 @@
-function firstNonRepeating(str){
-    let obj={}
-    for(let element of str){
-      obj[element]=(obj[element] || 0)+1;
-      
-    }
-    
-    for(let i=0;i<str.length;i++){
-      if(obj[str[i]]===1){
-        return i
-      }
-    }
-    return -1
-    
+function firstNonRepeating(str) {
+  let obj = {};
+  for (let element of str) {
+    obj[element] = (obj[element] || 0) + 1;
   }
-  const input="lovelov";
-  console.log(firstNonRepeating(input));
+
+  for (let i = 0; i < str.length; i++) {
+    if (obj[str[i]] === 1) {
+      return i;
+    }
+  }
+  return -1;
+}
+const input = "lovelovfknfknfnkf";
+
+console.log(firstNonRepeating(input));
